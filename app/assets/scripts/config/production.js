4ec82410487f815fa90d2227b3e99a9082bca6d1
Olaf Veerman <olaf.veerman@gmail.com>
2017-03-23 21:29:23 +0000

    Prototype of grid explorer

---
+++ app/assets/scripts/config/production.js
@@ -0,0 +1,9 @@
+'use strict';
+import logo from './logo';
+
+export default {
+  environment: 'production',
+  consoleMessage: logo,
+  mapboxToken: 'pk.eyJ1IjoiZ3NkcG0iLCJhIjoiY2lqbmN5eG9mMDBndHVmbTU5Mmg1djF6MiJ9.QqFCD7tcmccysN8GUClW8w',
+  dataLayer: 'https://test-offgrid-mvt.s3.amazonaws.com/tiles/2a2d2321-a22c-486f-9f0e-92a92578e63c-4ee5a17a-fc30-4e96-8ca5-68fc99db5813/data.tilejson'
+};
---
+++ app/assets/scripts/main.js
@@ -0,0 +1,78 @@
+'use strict';
+import config from './config';
+
+console.log.apply(console, config.consoleMessage);
+console.log('Environment', config.environment);
+
+mapboxgl.accessToken = config.mapboxToken;
+var gridNetwork = config.dataLayer;
+var nav = new mapboxgl.NavigationControl();
+
+if (!mapboxgl.supported()) {
+  document.getElementById('map').innerHTML = 'Your browser does not support Mapbox GL';
+} else {
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [60, 2.867],
+    zoom: 1.5
+  })
+  attachDataToMap(map, gridNetwork)
+}
+
+function attachDataToMap(theMap, tilejson) {
+  theMap.on('load', function () {
+    fetch(tilejson)
+    .then(function (response) {
+      return response.json()
+    }).then(function (tilejson) {
+      var layer = tilejson;
+      layer.type = 'vector'
+      theMap.addSource('data', layer)
+      theMap.fitBounds([[layer.bounds[0], layer.bounds[1]], [layer.bounds[2], layer.bounds[3]]], {padding: 20})
+      theMap.addLayer({
+        'id': 'data',
+        'type': 'line',
+        'source': 'data',
+        'source-layer': 'data_layer',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': {
+            'property': 'voltage_kV',
+            'type': 'interval',
+            'stops': [
+              // LV
+              [0, 'blue'],
+              // MV
+              [1, 'green'],
+              // HV
+              [66, 'red'],
+              // UHV
+              [225, 'black']
+            ]
+          },
+          'line-width': 1
+        }
+      })
+      theMap.addControl(nav, 'top-left');
+    })
+  })
+};
+
+document.getElementById('filter').addEventListener('click',function(e) {
+  if(e.target && e.target.className == 'status-filter') {
+    var clickedLayer = e.target.innerText;
+    e.preventDefault();
+
+    if (clickedLayer === 'All') {
+      map.setFilter('data', ['has', 'status']);
+    } else {
+      map.setFilter('data', ['==', 'status', clickedLayer]);
+    }
+    document.querySelectorAll('.status-filter').forEach(function(o) {o.classList.remove('active')})
+    e.target.classList.add('active')
+  }
+});
