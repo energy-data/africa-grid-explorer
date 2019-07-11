fdae175739db10799c81d54339fa9fda06bdedfe
Jodie Gardiner <jodiegardiner@outlook.com>
2019-07-11 16:16:38 +0100

    Update target dataset url

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -5,5 +5,5 @@ export default {
   environment: 'production',
   consoleMessage: logo,
   mapboxToken: 'pk.eyJ1IjoiZ3NkcG0iLCJhIjoiY2lqbmN5eG9mMDBndHVmbTU5Mmg1djF6MiJ9.QqFCD7tcmccysN8GUClW8w',
-  energydataDataset: 'africa-electricity-transmission-and-distribution-2017'
+  energydataDataset: 'africa-electricity-transmission-and-distribution-grid-map-2017'
 };
