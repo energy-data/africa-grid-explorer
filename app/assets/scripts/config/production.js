a483ea9829afcef08b7b0f1d7e820b781160b4c7
Jodie Gardiner <jodiegardiner@outlook.com>
2021-05-10 13:20:22 +0100

    Update production.js

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,6 +4,6 @@ import logo from './logo';
 export default {
   environment: 'production',
   consoleMessage: logo,
-  mapboxToken: 'pk.eyJ1IjoiZ3NkcG0iLCJhIjoiY2lqbmN5eG9mMDBndHVmbTU5Mmg1djF6MiJ9.QqFCD7tcmccysN8GUClW8w',
+  mapboxToken: 'pk.eyJ1IjoiZ3NkcG0iLCJhIjoiY2p2MmZscXB2MjR3dTRkbGpkZ3pva3g0dyJ9.u-P7Pl9qnHCZiQj-2oRzFQ',
   energydataDataset: 'africa-electricity-transmission-and-distribution-grid-map-2017'
 };
